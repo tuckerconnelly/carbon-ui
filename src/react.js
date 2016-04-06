b2d8bc339d1daeacb95992abd7ab53d9e423b448
Tucker Connelly <web@tuckerconnelly.com>
2016-04-05 21:42:02 -0400

    Fix universal

--- src/react.js
+++ src/react.js
@@ -2,9 +2,16 @@
 
 let React
 
-// If running on the server, import react-native-web for SSR instead of
-// react-native
-if (process) React = require('react-native-web')
+// HACK if(process) means if(we're running in node aka the server)
+// Need to check if we're running in server for SSR--SSR needs
+// react-native-web, not react-native.
+// We could have the consumer (the app) compile carbon-ui, but
+// don't want to have that requirement.
+// react native packager also runs in node, so process will also
+// exist in react-native. To make sure we're in the server and not
+// react-native, we add !global.__BUNDLE_START_TIME__, which the
+// react native packager sets
+if (process && !global.__BUNDLE_START_TIME__) React = require('react-native-web')
 else React = require('react-native')
 
 export default React

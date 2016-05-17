a24e3ae8a3aa623ead48acce49c9be2784b6f80c
Tucker Connelly <web@tuckerconnelly.com>
2016-05-17 13:42:06 -0400

    Add lineHeights to styles/Type

--- src/styles/Type.js
+++ src/styles/Type.js
@@ -6,53 +6,67 @@ export default {
     color: Colors.blackPrimary,
     fontFamily: 'Roboto-Light',
     fontSize: 112,
+    // NOTE Not currently in MD spec, making educated guess
+    lineHeight: 112,
   },
   display3: {
     color: Colors.blackPrimary,
     fontFamily: 'Roboto-Regular',
     fontSize: 56,
+    // NOTE Not in MD spec
+    lineHeight: 56,
   },
   display2: {
     color: Colors.blackPrimary,
     fontFamily: 'Roboto-Regular',
     fontSize: 45,
+    lineHeight: 48,
   },
   display1: {
     color: Colors.blackPrimary,
     fontFamily: 'Roboto-Regular',
     fontSize: 34,
+    lineHeight: 40,
   },
   headline: {
     color: Colors.blackPrimary,
     fontFamily: 'Roboto-Regular',
     fontSize: 24,
+    lineHeight: 32,
   },
   title: {
     color: Colors.blackPrimary,
     fontFamily: 'Roboto-Medium',
     fontSize: 20,
+    // NOTE Not in MD spec
+    lineHeight: 28,
   },
   subheading: {
     color: Colors.blackPrimary,
     fontFamily: 'Roboto-Regular',
     fontSize: 16,
+    lineHeight: 24,
     [Breakpoints.ml]: { fontSize: 15 },
   },
   body2: {
     color: Colors.blackPrimary,
     fontFamily: 'Roboto-Medium',
     fontSize: 14,
+    lineHeight: 24,
     [Breakpoints.ml]: { fontSize: 13 },
   },
   body1: {
     color: Colors.blackPrimary,
     fontFamily: 'Roboto-Regular',
     fontSize: 14,
+    lineHeight: 20,
     [Breakpoints.ml]: { fontSize: 13 },
   },
   caption: {
     color: Colors.blackPrimary,
     fontFamily: 'Roboto-Regular',
     fontSize: 12,
+    // NOTE Not in MD spec
+    lineHeight: 16,
   },
 }

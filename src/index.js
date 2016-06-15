dde8d65e695daa255128615e0794d1d84e4e4f78
Tucker Connelly <web@tuckerconnelly.com>
2016-06-15 19:01:20 -0400

    Allow importing directly from carbon-ui

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
-export { default as ThemeProvider } from './ThemeProvider'
+export * from './styles'
+
 export { default as connectTheme } from './connectTheme'
+export { default as ThemeProvider } from './ThemeProvider'
+
+export { default as Button } from './Button'
+export { default as Divider } from './Divider'
+export { default as Error } from './Error'
+export { default as Icon } from './Icon'
+export { default as Input } from './Input'
+export { default as Paper } from './Paper'
+export { default as TouchableRipple } from './TouchableRipple'
+export * from './Type'

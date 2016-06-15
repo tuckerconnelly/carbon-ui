27cf1ac62a3716fa04f9574980d0771719ed3ab5
Tucker Connelly <web@tuckerconnelly.com>
2016-06-15 18:59:22 -0400

    Convert Type components to class so they have have refs

--- src/Type.js
+++ src/Type.js
@@ -1,46 +1,97 @@
-import React, { PropTypes } from 'react'
+/* eslint-disable react/no-multi-comp */
+
+import React, { PropTypes, Component } from 'react'
 import { Text } from 'react-native-universal'
 import Uranium from 'uranium'
 
 import styles from './styles/Type'
 
-export const Display4 = ({ children, style, ...other }) =>
-  <Text style={[styles.display4, style]} {...other}>{children}</Text>
+// Using Class instead of function because refs can't be applied to function
+// components
 
-export const Display3 = ({ children, style, ...other }) =>
-  <Text style={[styles.display3, style]} {...other}>{children}</Text>
+class Display4 extends Component {
+  render() {
+    const { children, style, ...other } = this.props
+    return <Text style={[styles.display4, style]} {...other}>{children}</Text>
+  }
+}
 
-export const Display2 = ({ children, style, ...other }) =>
-  <Text style={[styles.display2, style]} {...other}>{children}</Text>
+class Display3 extends Component {
+  render() {
+    const { children, style, ...other } = this.props
+    return <Text style={[styles.display3, style]} {...other}>{children}</Text>
+  }
+}
 
-export const Display1 = ({ children, style, ...other }) =>
-  <Text style={[styles.display1, style]} {...other}>{children}</Text>
+class Display2 extends Component {
+  render() {
+    const { children, style, ...other } = this.props
+    return <Text style={[styles.display2, style]} {...other}>{children}</Text>
+  }
+}
 
-export const Headline = ({ children, style, ...other }) =>
-  <Text style={[styles.headline, style]} {...other}>{children}</Text>
+class Display1 extends Component {
+  render() {
+    const { children, style, ...other } = this.props
+    return <Text style={[styles.display1, style]} {...other}>{children}</Text>
+  }
+}
 
-export const Title = ({ children, style, ...other }) =>
-  <Text style={[styles.title, style]} {...other}>{children}</Text>
+class Headline extends Component {
+  render() {
+    const { children, style, ...other } = this.props
+    return <Text style={[styles.headline, style]} {...other}>{children}</Text>
+  }
+}
 
-export const Subheading = Uranium(({ children, css, ...other }) =>
-  <Text css={[styles.subheading, css]} {...other}>{children}</Text>
-)
+class Title extends Component {
+  render() {
+    const { children, style, ...other } = this.props
+    return <Text style={[styles.title, style]} {...other}>{children}</Text>
+  }
+}
 
-export const Body2 = Uranium(({ children, css, ...other }) =>
-  <Text css={[styles.body2, css]} {...other}>{children}</Text>
-)
+class Subheading extends Component {
+  render() {
+    const { children, css, ...other } = this.props
+    return <Text css={[styles.subheading, css]} {...other}>{children}</Text>
+  }
+}
 
-export const Body1 = Uranium(({ children, css, ...other }) =>
-  <Text css={[styles.body1, css]} {...other}>{children}</Text>
-)
+const UraniumSubheading = Uranium(Subheading)
 
-export const Caption = ({ children, style, ...other }) =>
-  <Text style={[styles.caption, style]} {...other}>{children}</Text>
+class Body2 extends Component {
+  render() {
+    const { children, css, ...other } = this.props
+    return <Text css={[styles.body2, css]} {...other}>{children}</Text>
+  }
+}
 
+const UraniumBody2 = Uranium(Body2)
+
+class Body1 extends Component {
+  render() {
+    const { children, css, ...other } = this.props
+    return <Text css={[styles.body1, css]} {...other}>{children}</Text>
+  }
+}
+
+const UraniumBody1 = Uranium(Body1)
+
+class Caption extends Component {
+  render() {
+    const { children, style, ...other } = this.props
+    return <Text style={[styles.caption, style]} {...other}>{children}</Text>
+  }
+}
 
 const propTypes = {
   children: PropTypes.node,
   style: PropTypes.oneOfType([
+    PropTypes.object,
+    PropTypes.array,
+  ]),
+  css: PropTypes.oneOfType([
     PropTypes.object,
     PropTypes.array,
   ]),
@@ -56,3 +107,16 @@ Subheading.propTypes = propTypes
 Body2.propTypes = propTypes
 Body1.propTypes = propTypes
 Caption.propTypes = propTypes
+
+export {
+  Display4,
+  Display3,
+  Display2,
+  Display1,
+  Headline,
+  Title,
+  UraniumSubheading as Subheading,
+  UraniumBody2 as Body2,
+  UraniumBody1 as Body1,
+  Caption,
+}

3c4bfac1a0e3979be7dd138ca075603c7b0358ae
Tucker Connelly <web@tuckerconnelly.com>
2016-11-13 14:33:45 -0500

    Fix IconToggle on android

--- src/IconToggle.js
+++ src/IconToggle.js
@@ -5,14 +5,20 @@ import { TouchableRipple, Icon, gu } from './index'
  * Icons are appropriate for toggle buttons that allow a single choice to be
  * selected or deselected, such as adding or removing a star to an item.
  */
-const IconToggle = ({ iconName, style, iconStyle, ...other }) =>
-  <TouchableRipple
-    rippleCentered
-    hitSlop={{ top: 3 * gu, right: 3 * gu, bottom: 3 * gu, left: 3 * gu }}
-    style={[styles.base, style]}
-    {...other}>
-    <Icon name={iconName} style={iconStyle} />
-  </TouchableRipple>
+const IconToggle = ({ iconName, style, iconStyle, ...other }) => { // eslint-disable-line arrow-body-style, max-len
+  // Implementing hitslop manually and reducing rippleSpread to get around
+  // no overflow: visible in android:
+  // https://github.com/facebook/react-native/issues/3198
+  return (
+    <TouchableRipple
+      rippleCentered
+      rippleSpread={0.67}
+      style={[styles.base, style]}
+      {...other}>
+      <Icon name={iconName} style={iconStyle} />
+    </TouchableRipple>
+  )
+}
 
 IconToggle.propTypes = {
   /**
@@ -34,6 +40,11 @@ export default IconToggle
 
 const styles = {
   base: {
+    width: 9 * gu,
+    height: 9 * gu,
+
+    alignItems: 'center',
+    justifyContent: 'center',
     overflow: undefined,
   },
 }

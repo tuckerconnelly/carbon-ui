857c4a48a0b39e6e6449437eb6fd02a9e6f519f0
Tucker Connelly <web@tuckerconnelly.com>
2016-11-20 12:05:01 -0500

    Add support for nested ListItems

--- src/ListItem.js
+++ src/ListItem.js
@@ -1,6 +1,7 @@
 import React, { Component, PropTypes } from 'react'
-import { Animated } from 'react-native-universal'
+import { Animated, View } from 'react-native-universal'
 import Uranium, { animate } from 'uranium'
+import omit from 'lodash/omit'
 import {
   Icon,
   Subheading,
@@ -14,6 +15,7 @@ import {
 } from './index'
 
 const HOVER_FADE_DURATION = 175
+const EXPAND_DURATION = 150
 
 /**
  * Individual items for the <List /> component
@@ -21,7 +23,8 @@ const HOVER_FADE_DURATION = 175
 class ListItem extends Component {
   state = { hovered: false }
 
-  componentDidUpdate(_, prevState) {
+  componentDidUpdate(prevProps, prevState) {
+    const { expanded } = this.props
     const { hovered } = this.state
 
     if (!prevState.hovered && hovered) {
@@ -29,39 +32,88 @@ class ListItem extends Component {
     } else if (prevState.hovered && !hovered) {
       Animations.standard(this._hoverAV, 0, HOVER_FADE_DURATION).start()
     }
+
+    if (!prevProps.expanded && expanded) {
+      Animations.standard(this._expandIconAV, 1, EXPAND_DURATION).start()
+      Animations.staggered(this._expandHeightAV, this._expandOpacityAV, 1, EXPAND_DURATION).start()
+    } else if (prevProps.expanded && !expanded) {
+      Animations.standard(this._expandIconAV, 0, EXPAND_DURATION).start()
+      Animations.staggered(this._expandHeightAV, this._expandOpacityAV, 0, EXPAND_DURATION).start()
+    }
   }
 
   _hoverAV = new Animated.Value(0)
+  _expandIconAV = new Animated.Value(this.props.expanded ? 1 : 0)
+  _expandHeightAV = new Animated.Value(this.props.expanded ? 1 : 0)
+  _expandOpacityAV = new Animated.Value(this.props.expanded ? 1 : 0)
 
   render() {
-    const { leftIcon, primaryText, active, theme, ...other } = this.props
+    const AnimatedIcon = Animated.createAnimatedComponent(Icon)
 
-    const tStyles = styles(theme)
+    const { leftIcon, primaryText, active, onPress, children, style, theme, ...other } = this.props
+
+    const styles = tStyles(theme)
+    const childrenCount = React.Children.count(children)
+    const otherWithoutCustomProps = omit(other, 'expanded')
 
     return (
-      <TouchableRipple
-        onMouseEnter={() => this.setState({ hovered: true })}
-        onMouseLeave={() => this.setState({ hovered: false })}
-        {...other}>
-        <Animated.View
-          css={tStyles.base}
-          style={animate(['backgroundColor'], tStyles.base, tStyles.hovered, this._hoverAV)}>
-          {leftIcon &&
-            <Icon
-              name={leftIcon}
-              style={[
-                tStyles.icon,
-                active && tStyles.active,
-              ]} />
-          }
-          <Subheading
-            style={active ? tStyles.active : undefined}
-            numberOfLines={1}
-            ellipsizeMode="tail">
-            {primaryText}
-          </Subheading>
-        </Animated.View>
-      </TouchableRipple>
+      <View>
+        <TouchableRipple
+          onMouseEnter={() => this.setState({ hovered: true })}
+          onMouseLeave={() => this.setState({ hovered: false })}
+          onPress={onPress}>
+          <Animated.View
+            css={styles.base}
+            style={[
+              animate(['backgroundColor'], styles.base, styles.hovered, this._hoverAV),
+            ].concat(style)}
+            {...otherWithoutCustomProps}>
+            {leftIcon &&
+              <Icon
+                name={leftIcon}
+                style={[
+                  styles.leftIcon,
+                  active && styles.active,
+                ]} />
+            }
+            <Subheading
+              style={active ? styles.active : undefined}
+              numberOfLines={1}
+              ellipsizeMode="tail">
+              {primaryText}
+            </Subheading>
+            {childrenCount > 0 &&
+              <AnimatedIcon
+                name="keyboard_arrow_down"
+                css={styles.rightIcon}
+                style={animate(
+                  styles.expandIconCollapsed,
+                  styles.expandIconExpanded,
+                  this._expandIconAV
+                )} />
+            }
+          </Animated.View>
+        </TouchableRipple>
+        {childrenCount > 0 &&
+          <Animated.View
+            style={[
+              styles.nestedList,
+              animate('maxHeight', 0, (childrenCount * styles.base.height) + 40, this._expandHeightAV),
+              animate('opacity', 0, 1, this._expandOpacityAV),
+            ]}>
+            {/* Man, all this just to add padding to children elements */}
+            {React.Children.map(children, listItem =>
+              React.cloneElement(listItem, {
+                ...listItem.props,
+                style: {
+                  ...styles.nestedListItem,
+                  ...listItem.props.style,
+                },
+              })
+            )}
+          </Animated.View>
+        }
+      </View>
     )
   }
 }
@@ -79,6 +131,16 @@ ListItem.propTypes = {
    * `true` if the list item is currently selected
    */
   active: PropTypes.bool,
+  /**
+   * Controls the expanded/collapses state if there are ListItem children
+   */
+  expanded: PropTypes.bool,
+  /**
+   * Pass additional ListItems as children to make this ListItem expandable
+   */
+  children: PropTypes.node,
+  style: PropTypes.oneOfType([PropTypes.object, PropTypes.array]),
+  onPress: PropTypes.func,
 
   // connectTheme
   theme: PropTypes.object.isRequired,
@@ -89,12 +151,14 @@ export default
   Uranium(
   ListItem))
 
-const styles = theme => ({
+const tStyles = theme => ({
   base: {
-    justifyContent: 'center',
-
     height: 12 * gu,
     paddingHorizontal: 4 * gu,
+
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    flexDirection: 'row',
 
     backgroundColor: Colors.white,
 
@@ -103,8 +167,26 @@ const styles = theme => ({
     },
   },
 
-  icon: {
+  leftIcon: {
     marginRight: 8 * gu,
+  },
+
+  rightIcon: {
+    position: 'absolute',
+    right: 4 * gu,
+    top: 3 * gu,
+
+    [Breakpoints.ml]: {
+      top: 2 * gu,
+    },
+  },
+
+  expandIconCollapsed: {
+    transform: [{ rotateZ: '0deg' }],
+  },
+
+  expandIconExpanded: {
+    transform: [{ rotateZ: '-180deg' }],
   },
 
   hovered: {
@@ -113,5 +195,13 @@ const styles = theme => ({
 
   active: {
     color: theme.primary,
+  },
+
+  nestedList: {
+    overflow: 'hidden',
+  },
+
+  nestedListItem: {
+    paddingLeft: 18 * gu,
   },
 })

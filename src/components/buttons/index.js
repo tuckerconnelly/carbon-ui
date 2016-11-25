9d25a38e5ef8d4daf3140201cdb0cecc4a169299
Tucker Connelly <web@tuckerconnelly.com>
2016-11-25 11:49:07 -0500

    Reorganize everything

---
+++ src/components/buttons/index.js
@@ -0,0 +1,3 @@
+export { default as FlatButton } from './FlatButton'
+export { default as IconToggle } from './IconToggle'
+export { default as RaisedButton } from './RaisedButton'
---
+++ src/theme/index.js
@@ -0,0 +1,4 @@
+export { default as themes } from './themes'
+
+export { default as connectTheme } from './connectTheme'
+export { default as ThemeProvider } from './ThemeProvider'

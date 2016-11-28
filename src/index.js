d7dbc79cd21b4cc75d26986f5b0661c1e5361f4e
Tucker Connelly <web@tuckerconnelly.com>
2016-11-28 13:37:35 -0500

    Polyfill matchMedia for exponent

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+import { matchMediaMock } from 'uranium'
+
+// Polyfill matchMedia for exponent
+if (global.__exponent && !global.matchMedia) global.matchMedia = matchMediaMock
+
 export * from './style'
 export * from './theme'
 export * from './components'

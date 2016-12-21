1a65db47b1f169c3c783f54f0379e1e855991e2e
Tucker Connelly <web@tuckerconnelly.com>
2016-12-21 08:51:18 -0500

    Let Uranium handle matchMediaMock

--- src/index.js
+++ src/index.js
@@ -1,8 +1,3 @@
-import { matchMediaMock } from 'uranium'
-
-// Polyfill matchMedia for exponent
-if (global.__exponent && !global.matchMedia) global.matchMedia = matchMediaMock
-
 export * from './style'
 export * from './theme'
 export * from './components'
